877ffd2b823c1497106dcf7f633cb652798ee2fe
Alexey Palshin <a.palshin@belitsoft.com>
2018-11-20 17:25:18 +0300

    publish config file

--- src/Providers/LaraminLaravelServiceProvider.php
+++ src/Providers/LaraminLaravelServiceProvider.php
@@ -7,5 +7,15 @@ use Illuminate\Support\ServiceProvider;
 
 class LaraminLaravelServiceProvider extends ServiceProvider
 {
-
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/laramin.php' => config_path('laramin.php'),
+        ]);
+    }
 }
