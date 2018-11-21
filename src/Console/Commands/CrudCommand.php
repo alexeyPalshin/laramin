811d83017f08abecd7d1518db4961733bfad36e2
Alexey Palshin <mykorespondencia@gmail.com>
2018-11-21 23:55:11 +0300

    added crud console command

---
+++ src/Console/Commands/CrudCommand.php
@@ -0,0 +1,26 @@
+`<?php
+
+
+namespace Laramin\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CrudCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crud:hello';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->output->write(['hello'], true);
+    }
+}
--- src/config/laramin.php
+++ src/config/laramin.php
@@ -1,1 +1,18 @@
 <?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Administration entry point
+    |--------------------------------------------------------------------------
+    |
+    | This option controls url address that will be used to log into the admin
+    | part of the application. You may change these defaults as required, but
+    | they're a perfect start for most applications.
+    |
+    */
+
+    'entrypoint' => 'admin'
+
+];
